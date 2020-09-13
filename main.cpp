b8c8945e858c60aeb664b6826f73d9319a2c19c3
thethanosdaddy <thethanosdaddy@gmail.com>
2020-09-13 10:54:43 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include "tree.h"
+#include "heap.h"
 
 int main() 
 {
-    Tree<int> tree(10);
-    tree.Insert(9);
-    tree.Insert(3);
-    tree.Insert(55);
-    tree.Insert(12);
-    tree.Insert(11);
-    tree.Print();
+    Heap<int> heap;
+    heap.Insert(9);
+    heap.Insert(3);
+    heap.Insert(55);
+    heap.Insert(12);
+    heap.Insert(11);
+    heap.Print();
 
 
     return 0;
