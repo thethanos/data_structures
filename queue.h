7577c84e571e707e1aae9c0159d42e709f879ab9
thethanos <thethanosdaddy@gmail.com>
2020-11-09 12:31:06 +0300

    queue

---
+++ queue.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "dllist.h"
+
+template <typename T, typename Container = DLL::DLList<T>>
+class Queue
+{
+public:
+	Queue() {}
+	~Queue() {}
+
+public:
+	void push(T value) { m_Data.push_back(value); }
+	void pop()   { m_Data.pop_front();    }
+	bool empty() { return m_Data.empty(); }
+	uint size()  { return m_Data.size();  }
+	T front()    { return m_Data.front(); }
+	T back()     { return m_Data.back();  }
+
+private:
+	Container m_Data;
+};
