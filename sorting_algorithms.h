f4ce010437b5ae745bfe96aaa10b730bd8abbdf1
thethanos <thethanosdaddy@gmail.com>
2020-12-22 20:03:23 +0300

    sorting algorithms

--- sorting_algorithms.h
+++ sorting_algorithms.h
@@ -1,17 +1,69 @@
 #pragma once
 
-#include <algorithm>
+#include <iostream>
 
 namespace util
 {
-	template <class T>
-	void swap(T& begin, T& end)
+	template <typename T, size_t size>
+	void print_array(T(&arr)[size])
 	{
-		T temp = begin;
-		begin  = end;
-		end    = temp;
+		for (int i(0); i < size; ++i)
+			std::cout << arr[i] << std::endl;
 	}
 
+	template <class T>
+	void swap(T& first, T& second)
+	{
+		T temp = first;
+		first  = second;
+		second = temp;
+	}
+
+	void merge(int*, int, int, int);
+	void merge_sort(int* arr, int low, int high)
+	{
+		if (low >= high)
+			return;
+			
+		int mid = (low + high) / 2;
+		merge_sort(arr, low, mid);
+		merge_sort(arr, mid + 1, high);
+			
+		merge(arr, low, high, mid);
+	}
+
+	void merge(int* arr, int low, int high, int mid)
+	{
+		int i(low), j(mid + 1), k(low);
+		int* temp = new int[high + 1];
+
+		while (i <= mid && j <= high)
+		{
+			if (arr[i] < arr[j])
+			{
+				temp[k] = arr[i];
+				k++;
+				i++;
+			}
+			else
+			{
+				temp[k] = arr[j];
+				k++;
+				j++;
+			}
+		}
+
+		for (; i <= mid; k++, i++)
+			temp[k] = arr[i];
+
+		for (; j <= high; k++, j++)
+			temp[k] = arr[j];
+
+		for (i = low; i < k; i++)
+			arr[i] = temp[i];
+
+		delete[] temp;
+	}
 }
 
 template <typename T, size_t size>
@@ -22,7 +74,7 @@ void bubble_sort(T (&arr)[size])
 	{
 		for (int j(0); j < size - 1; ++j)
 		{
-			if (arr[j] > arr[j + 1])
+			if (util::swap(arr[j], arr[j + 1]))
 			{
 				util::swap(arr[j], arr[j + 1]);
 				early_exit = false;
@@ -49,7 +101,7 @@ void selection_sort(T(&arr)[size])
 }
 
 template <typename T, size_t size>
-void coctail_sort(T(&arr)[size])
+void cocktail_sort(T(&arr)[size])
 {
 	int begin = 0, end = size - 1;
 	bool swapped = true;
@@ -87,4 +139,8 @@ void coctail_sort(T(&arr)[size])
 	}
 }
 
-
+template <typename T, size_t size>
+void merge_sort(T(&arr)[size])
+{
+	util::merge_sort(arr, 0, size - 1);
+}
