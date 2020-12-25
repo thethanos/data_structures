74f35b5864fc4ef59eddeb1716caba379cd648ed
thethanos <thethanosdaddy@gmail.com>
2020-12-25 22:25:56 +0300

    stack

--- stack.h
+++ stack.h
@@ -11,10 +11,10 @@ public:
 
 public:
 	void push(T value) { m_Data.push_back(value); }
-	void pop() { m_Data.pop_back(); }
-	T	 top() { return m_Data.back(); }
+	void pop()   { m_Data.pop_back(); }
+	T	 top()   { return m_Data.back(); }
 	bool empty() { return m_Data.empty(); }
-	uint size() { return m_Data.size(); }
+	uint size()  { return m_Data.size(); }
 
 private:
 	Container m_Data;
