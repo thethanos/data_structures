23d3c127b4e7b94607d288642a25ff8e8b15b40a
thethanos <thethanosdaddy@gmail.com>
2021-01-17 17:50:53 +0300

    dijkstra's algorithm

--- dijkstra_algorithm.h
+++ dijkstra_algorithm.h
@@ -118,27 +118,38 @@ public:
     void add_edge(int from, int to, int weight);
     void dijkstra_algorithm(int start);
 
+    vector<int> get_path(int from, int to);
+
+private:
+    void get_path(vector<int>& path, int index);
+
 private:
     vector<list<pair<int, int>>> m_Data;
-    vector<int>                 m_Visited;
-    vector<int>                 m_Distances;
+    vector<bool>                 m_Visited;
+    vector<int>                  m_Distances;
+    vector<int>                  m_Path;
 };
 
 void UGraphAL::add_edge(int from, int to, int weight)
 {
-    m_Data[from].push_back(pair{weight, to});
-    m_Data[to].push_back(pair{ weight, from });
+    m_Data[from].push_back(std::make_pair(weight, to));
+    m_Data[to].push_back(std::make_pair(weight, from));
 }
 
 void UGraphAL::dijkstra_algorithm(int start)
-{
+{   
+    m_Distances.clear();
+    m_Visited.clear();
+    m_Path.clear();
+
     m_Distances.resize(m_Data.size(), INT_MAX);
     m_Visited.resize(m_Data.size());
+    m_Path.resize(m_Data.size(), -1);
 
     m_Distances[start] = 0;
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, std::greater<pair<int, int>>> que;
-    que.push(pair{ 0, start });
+    que.push(std::make_pair( 0, start ));
 
     int cur_vert(0);
     while (!que.empty())
@@ -154,10 +165,32 @@ void UGraphAL::dijkstra_algorithm(int start)
 
             if (m_Distances[cur_vert] + adj_vert.first < m_Distances[adj_vert.second])
             {
+                m_Path[adj_vert.second] = cur_vert;
                 m_Distances[adj_vert.second] = m_Distances[cur_vert] + adj_vert.first;
-                adj_vert.first = m_Distances[cur_vert] + adj_vert.first;
-                que.push(adj_vert);
+                que.push(std::make_pair(m_Distances[adj_vert.second], adj_vert.second));
             }
         }
     }
+}
+
+vector<int> UGraphAL::get_path(int from, int to)
+{
+    dijkstra_algorithm(from);
+
+    vector<int> path;
+    path.push_back(from);
+
+    get_path(path, to);
+
+    return path;
+}
+
+void UGraphAL::get_path(vector<int>& path, int index)
+{
+    if (m_Path[index] == -1)
+        return;
+
+    get_path(path, m_Path[index]);
+
+    path.push_back(index);
 }
