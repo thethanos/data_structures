34104706f17acea1bcbdc0e019a3f66e2530e7ed
thethanos <thethanosdaddy@gmail.com>
2021-01-21 22:44:58 +0300

    dynamic array

--- darray.h
+++ darray.h
@@ -1,22 +1,60 @@
 #pragma once
 
-#include <algorithm>
 
+namespace DA
+{
+	template <typename T>
+	class Allocator
+	{
+	public:
+		static T*	allocate(size_t n); 
+		static void	deallocate(T* ptr, size_t n = 0) noexcept { operator delete(ptr); }
 
-template <typename T>
+		template <typename T, typename ... Args>
+		static void construct(T* ptr, Args&& ... args)
+		{
+			try { new (ptr) T(std::forward<Args>(args)...); }
+			catch (...) {}
+		}
+		static void destroy(T* ptr) noexcept { ptr->~T(); }
+	};
+
+	template <typename T>
+	T* Allocator<T>::allocate(size_t n)
+	{
+		try{ return static_cast<T*>(operator new(n * sizeof(T))); }
+		catch (...){}
+
+		return nullptr;
+	}
+}
+
+template <typename T, typename Allocator = DA::Allocator<T>>
 class DArray
 {
 public:
 	DArray() {}
-	DArray(DArray<T>& other)  { *this = other; }
-	DArray(DArray<T>&& other) { *this = std::move(other); }
+	DArray(const DArray<T, Allocator>& other)  { *this = other; }
+	DArray(DArray<T, Allocator>&& other) { *this = std::move(other); }
 	DArray(const std::initializer_list<T>& lst);
 	DArray(size_t size) { resize(size, T()); }
-	~DArray() { delete[] m_pData; }
+	~DArray();
 
 public:
 	void push_back(const T& value);
-	void pop_back() { --m_Size; }
+	void pop_back();
+
+	template <typename ... Args>
+	void emplace_back(Args&& ... args)
+	{
+		if (m_Size == 0)
+			reserve(2);
+		else if (m_Size == m_Capacity)
+			reserve(m_Size * 2);
+
+		m_Allocator.construct(m_pData + m_Size, std::forward<Args>(args)...);
+		m_Size++;
+	}
 
 	bool   empty()	  const { return m_Size <= 0; }
 	size_t size()	  const { return m_Size; }
@@ -24,19 +62,21 @@ public:
 	T&	   back()	  const { return m_pData[m_Size - 1]; }
 
 	void resize(const size_t new_size);
-	void resize(const size_t new_size, const T value);
+	void resize(const size_t new_size, const T& value);
 	void reserve(const size_t new_capacity);
 
 public:
 	T& operator[](size_t index) { return m_pData[index]; }
 
-	DArray<T>& operator=(DArray<T>& other);
-	DArray<T>& operator=(DArray<T>&& other);
+	DArray<T, Allocator>& operator=(const DArray<T, Allocator>& other);
+	DArray<T, Allocator>& operator=(DArray<T, Allocator>&& other) noexcept;
 
 private:
 	T*     m_pData{ nullptr };
 	size_t m_Size{ 0 };
 	size_t m_Capacity{ 0 };
+
+	Allocator m_Allocator;
 
 public:
 	using iterator = T*;
@@ -46,75 +86,96 @@ public:
 	using value_type = T;
 };
 
-template <typename T>
-DArray<T>::DArray(const std::initializer_list<T>& lst)
+template <typename T, typename Allocator>
+DArray<T, Allocator>::DArray(const std::initializer_list<T>& lst)
 {
-	resize(lst.size());
-	std::copy(lst.begin(), lst.end(), begin());
+	m_pData = m_Allocator.allocate(lst.size());
+	for (int i(0); i < lst.size(); ++i)
+		m_Allocator.construct(m_pData + i, std::move(*(lst.begin() + i)));
+
+	m_Size = m_Capacity = lst.size();
 }
 
-template <typename T>
-void DArray<T>::push_back(const T& value)
+template <typename T, typename Allocator>
+DArray<T, Allocator>::~DArray()
+{
+	for (int i(0); i < m_Size; ++i)
+		m_Allocator.destroy(m_pData + i);
+
+	m_Allocator.deallocate(m_pData, m_Capacity);
+}
+
+template <typename T, typename Allocator>
+void DArray<T, Allocator>::push_back(const T& value)
 {
 	if(m_Size == 0) 
 		reserve(2);
 	else if(m_Size == m_Capacity) 
 		reserve(m_Size * 2);
 
-	m_pData[m_Size] = value;
+	m_Allocator.construct(m_pData+m_Size, value);
 	m_Size++;
 }
 
-template <typename T>
-void DArray<T>::resize(const size_t new_size)
+template <typename T, typename Allocator>
+void DArray<T, Allocator>::pop_back()
+{
+	m_Allocator.destroy(m_pData + m_Size);
+	m_Size--;
+}
+
+template <typename T, typename Allocator>
+void DArray<T, Allocator>::resize(const size_t new_size)
 {
 	if(new_size <= 0) return;
 
-	T* temp = new T[new_size];
+	T* temp = m_Allocator.allocate(new_size);
 
-	if(m_pData)
-		std::copy(begin(), end(), temp);
+	for (size_t i(0); i < m_Size; ++i)
+		m_Allocator.construct(temp + i, *(m_pData + i));
+
+	for (size_t i(m_Size); i < new_size; ++i)
+		m_Allocator.construct(temp + i);
+
+	for (size_t i(new_size); i < m_Size; ++i)
+		m_Allocator.destroy(m_pData + i);
+
+	m_Allocator.deallocate(m_pData, m_Capacity);
 
 	m_Size = m_Capacity = new_size;
-	delete[] m_pData;
 	m_pData = temp;
 }
 
-template <typename T>
-void DArray<T>::resize(size_t new_size, T value)
+template <typename T, typename Allocator>
+void DArray<T, Allocator>::resize(const size_t new_size, const T& value)
 {
-	if(new_size <= 0) return;
+	if (new_size <= 0) return;
 
 	size_t old_size = m_Size;
 	resize(new_size);
 
-	for(size_t counter(old_size); counter < m_Size; ++counter)
-		m_pData[counter] = value;
+	for (size_t i(old_size); i < new_size; ++i)
+		m_pData[i] = value;
 }
 
-template <typename T>
-void DArray<T>::reserve(size_t new_capacity)
+template <typename T, typename Allocator>
+void DArray<T, Allocator>::reserve(size_t new_capacity)
 {
 	if(new_capacity <= 0 || new_capacity <= m_Size) return;
 
-	if (new_capacity <= m_Capacity)
-	{
-		m_Capacity = new_capacity;
-		return;
-	}
+	T* temp = m_Allocator.allocate(new_capacity);
 
-	T* temp = new T[new_capacity];
+	for (int i(0); i < m_Size; ++i)
+		m_Allocator.construct(temp + i, *(m_pData + i));
 
-	if(m_pData)
-		std::copy(begin(), end(), temp);
+	m_Allocator.deallocate(m_pData, m_Capacity);
 
 	m_Capacity = new_capacity;
-	delete[] m_pData;
 	m_pData = temp;
 }
 
-template <typename T>
-DArray<T>& DArray<T>::operator=(DArray<T>& other)
+template <typename T, typename Allocator>
+DArray<T, Allocator>& DArray<T, Allocator>::operator=(const DArray<T, Allocator>& other)
 {
 	if (this == &other) return *this;
 
@@ -124,8 +185,8 @@ DArray<T>& DArray<T>::operator=(DArray<T>& other)
 	return *this;
 }
 
-template <typename T>
-DArray<T>& DArray<T>::operator=(DArray<T>&& other)
+template <typename T, typename Allocator>
+DArray<T, Allocator>& DArray<T, Allocator>::operator=(DArray<T, Allocator>&& other) noexcept
 {
 	if (this == &other) return *this;
 
