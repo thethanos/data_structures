d21bb4d6b1becb891e46f44f4a51340c72128282
thethanos <thethanosdaddy@gmail.com>
2021-03-15 21:12:06 +0300

    hash table

--- hashtable.h
+++ hashtable.h
@@ -3,20 +3,62 @@
 #include <vector>
 
 using std::vector;
-using uint = unsigned int;
 
-template <typename Key, typename Value>
+namespace HT 
+{
+	template <typename Key, typename Value>
+	struct Bucket
+	{
+		Bucket(Key key, Value value = Value()) :m_Key(key), m_Value(value) {}
+
+		Key   m_Key;
+		Value m_Value;
+
+		Bucket<Key, Value>* m_Next = nullptr;
+	};
+}
+
+template <typename Key, typename Value, typename Hash = std::hash<Key>>
 class HashTable
 {
 public:
-	HashTable(uint size) { m_Data.resize(size); }
+	HashTable(size_t size = 8) { m_Data.resize(size, nullptr); }
 
 public:
-	Value& operator[](Key key) { return m_Data[hash_function(key)]; }
+	void insert(const std::pair<Key, Value>& pair);
+	Value& operator[](const Key& key);
+
+	size_t size() { return m_Size; }
+	size_t bucket_count() { return m_Data.size(); }
 
 private:
-	uint hash_function(Key key) { return reinterpret_cast<uint>(key) % m_Data.size(); }
+	vector<HT::Bucket<Key, Value>*> m_Data;
+	Hash m_Hash;
+
+	size_t m_Size = 0;
 
 private:
-	vector<Value> m_Data;
+	size_t get_index(const Key& key) { return m_Hash(key) % bucket_count(); }
 };
+
+template <typename Key, typename Value, typename Hash>
+void HashTable<Key, Value, Hash>::insert(const std::pair<Key, Value>& pair)
+{
+	if (m_Size >= bucket_count())
+		m_Data.resize(bucket_count() * 8, nullptr);
+
+	size_t index = get_index(pair.first);
+
+	m_Data[index] = new HT::Bucket<Key, Value>(pair.first, pair.second);
+}
+
+template <typename Key, typename Value, typename Hash>
+Value& HashTable<Key, Value, Hash>::operator[](const Key& key)
+{
+	size_t index = get_index(key);
+
+	if (m_Data[index] == nullptr)
+		m_Data[index] = new HT::Bucket<Key, Value>(key);
+
+	return m_Data[index]->m_Value;
+}
